26f5ad4204851878316f111445b82024376648e7
Daniel Thomas <dan.thomas@playwerks.co.uk>
2022-01-26 13:53:13 +0000

    numbers and strings

--- 03_numbers.php
+++ 03_numbers.php
@@ -2,20 +2,84 @@
 
 // Declaring numbers
 
-// Arithmetic operations
+$a = 5;
+$b = 4;
+$c = 1.2;
+
+// Arithmetic operations uses BODMAS - brackets/Operators/Multiplication/Addition/Subtraction
+
+echo $a + $b . '</br>'; // will be equal to 9;
+echo $a + $b * $c . '</br>'; // will be equal to (b * c) + a;
+
+echo $a - $b . '</br>'; // Subtract
+echo $a * $b . '</br>'; // multiply
+echo $a / $b . '</br>'; // division
+echo $a % $b . '</br>'; //remainder / modulus
 
 // Assignment with math operators
 
+// $a = $a + $b; // CAN BE WRITTEN AS: 
+
+// $a += $b; // $a = $a + $b;
+// $a -= $b; // $a = $a - $b;
+// $a *= $b; // $a = $a * $b;
+// $a /= $b; // $a = $a / $b;
+// $a %= $b; // $a = $a % $b;
+
+echo $a . '</br>';
+
 // Increment operator
+
+echo $a++ . '</br>'; // increment $a by one - if used with another operator, $a is used / printed then incremented; $a = 5 now and 5 will show on the page, after printing $a will equal 6
+
+echo ++$a . '</br>'; // increment $a by one - if used with another operator, $a is incremented / printed then used; $a = 7 now and 7 will show on the page
 
 // Decrement operator
 
+$a = 5;
+
+echo $a-- . '</br>'; // decrement $a by one - if used with another operator, $a is used / printed then decremented; $a = 5 now and 5 will show on the page, after printing $a will equal 4
+
+echo --$a . '</br>'; // decrement $a by one - if used with another operator, $a is incremented / printed then used; $a = 3 now and 3 will show on the page
+
 // Number checking functions
+
+echo (is_float($a) ? 'true' : 'false') . '</br>';
+echo (is_float(1.25) ? 'true' : 'false') . '</br>';
+echo (is_int($b) ? 'true' : 'false') . '</br>';
+echo (is_integer($b) ? 'true' : 'false') . '</br>';
+echo (is_numeric("3.2") ? 'true' : 'false') . '</br>'; // is_numeric reads the string value and evaultes if it is a numeric value
 
 // Conversion
 
+$strNum = "12.34";
+
+$num = (int)$strNum; // converts to an integer appears as a rounded number
+$num = intval($strNum); // converts to an integer appears as a rounded number
+$num = (float)$strNum; // converts to a float / double
+
+echo '<pre>';
+var_dump($num);
+echo '<pre>';
+
 // Number functions
 
+echo "abs(-15) "   . abs(-15)   . '</br>';   // returns the absolute value
+echo "pow(2, 3) "  . pow(2, 3)  . '</br>';  // base, exponential
+echo "sqrt(16) "   . sqrt(16)   . '</br>';   // Square Root
+echo "max(2, 3) "  . max(2, 3)  . '</br>';  // Max value
+echo "min(2, 3) "  . min(2, 3)  . '</br>';  // Minimum Value
+echo "round(2.4) " . round(2.4) . '</br>'; // round regular
+echo "round(2.6) " . round(2.6) . '</br>'; // round regular
+echo "floor(2.6) " . floor(2.6) . '</br>'; // Round Down
+echo "ceil(2.4) "  . ceil(2.4)  . '</br>';  // Round Up
+
 // Formatting numbers
+
+$example = 123456789.12345;
+
+// number_format(number, decimals, decimal_point, thousands_separator);
+
+echo number_format($example, 2,) . '</br>';
 
 // https://www.php.net/manual/en/ref.math.php
--- 04_strings.php
+++ 04_strings.php
@@ -2,11 +2,51 @@
 
 // Create simple string
 
-// String concatenation
+$name = "Dave";
+
+// String concatenation / interpolation
+
+$string = 'Hello ' . $name; // single quotations;
+$string2 = "Hello $name"; // double quotation marks allows easy interpolation;
+
+echo $string . '</br>';
+echo $string2 . '</br>';
 
 // String functions
 
+$myStr = "     Hello World     ";
+
+echo "1 - strlen "         . strlen($myStr)         . '</br>'; // prints the length of the string including white spaces NOTE: html ignores white spaces. 
+echo "2 - trim "           . trim($myStr)           . '</br>'; // trims whitespaces on both sides
+echo "3 - ltrim "          . ltrim($myStr)          . '</br>'; // left trim
+echo "4 - rtrim "          . rtrim($myStr)          . '</br>'; // right trim
+echo "5 - str_word_count " . str_word_count($myStr) . '</br>'; // get the word count of a string
+echo "6 - strrev "         . strrev($myStr)         . '</br>'; // reverses the string
+echo "7 - strtoupper "     . strtoupper($myStr)     . '</br>'; // sets all characters to upper case
+echo "8 - strtolower "     . strtolower($myStr)     . '</br>'; // sets all characters to lower case
+echo "9 - ucfirst "        . ucfirst("hello world") . '</br>'; // sets first character to uppercase on first word
+echo "10 - lcfirst "       . lcfirst("Hello World") . '</br>'; // sets first character to lower case on first word
+echo "11 - ucwords "       . ucwords("hello world") . '</br>'; // sets first character on all words to uppercase
+echo "12 - strpos "        . strpos($myStr, 'World') . '</br>'; //  returns the starting position of the target word - case sensitive!
+echo "13 - stripos "       . stripos($myStr, 'world') . '</br>'; //  returns the starting position of the target word - case INsensitive!
+echo "14 - substr "        . substr($myStr, 11, 2)     . '</br>'; // create a substring. substr(string, target, offset);
+echo "15 - str_replace "   . str_replace('World', 'PHP', $myStr)     . '</br>'; // replace a word in a string str_replace(TARGET_WORD, NEW_VALUE, TARGET_STRING) - case sensitive
+echo "15 - str_ireplace "   . str_ireplace('world', 'PHP', $myStr)     . '</br>'; // replace a word in a string str_replace(TARGET_WORD, NEW_VALUE, TARGET_STRING) - case INsensitive
+
+
 // Multiline text and line breaks
+
+$longtext = "Hello my name is Dan
+            I work in Kelham Island
+            I was born in <strong>Sheffield</strong>";
+
+echo nl2br($longtext) . '</br>'; // nl2bt = NEW LINE TO LINE BREAK - creates a new line where a new line starts
+
+echo htmlentities($longtext) . '</br>'; // prints out the html tags contained in the string
+
+echo nl2br(htmlentities($longtext)) . '</br>'; // prints out the html tags contained in the string and preserves line breaks.
+
+echo html_entity_decode('&lt;b&gt;DAN&lt;/b&gt;'); // prints html from entity codes
 
 // Multiline text and reserve html tags
 
