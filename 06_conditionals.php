50720450c5720c1f84d6dbb465852ce48621823c
Daniel Thomas <dan.thomas@playwerks.co.uk>
2022-01-26 17:07:18 +0000

    conditionals

--- 06_conditionals.php
+++ 06_conditionals.php
@@ -5,20 +5,51 @@ $salary = 300000;
 
 // Sample if
 
-// Without circle braces
+if ($age > 19) {
+    echo "I AM OLDER THAN 20" . "</br>";
+}
+
+// Without circle braces if only one returning statement
+
+if ($age < 40) echo "I AM UNDER 40" . "</br>";
 
 // Sample if-else
 
-// Difference between == and ===
-
-// if AND
-
-// if OR
+if ($salary > 25000) {
+    echo "whoa" . "</br>";
+} else {
+    echo "Low paid" . "</br>";
+}
 
 // Ternary if
 
-// Short ternary
+echo ($age < 18 ? "no drinking" : "Boozey do") . "</br>";
+
+// Short ternary - need to look at this somemore!! essentially if the variable is not set do this!
+
+// echo $age ?: "Hello World" . "</br>"; 
 
 // Null coalescing operator
 
+$age = $age ?? "unknown" . "</br>";
+
+// echo $age . "</br>";
+
 // switch
+
+$age = 19;
+
+switch ($age) {
+
+    case 10:
+        echo "It's 10 or greater but less than 20";
+        break;
+
+    case 20:
+        echo "It's 20 or greater";
+        break;
+
+    default:
+        echo "what";
+        break;
+}
