4bfd849f3bbf008603d3cb49651fc3766fef503e
Daniel Thomas <dan.thomas@playwerks.co.uk>
2022-01-26 11:39:45 +0000

    syntax, comments and variables worked through

--- 01_comments.php
+++ 01_comments.php
@@ -1,1 +1,14 @@
 <?php
+
+
+// single line comment
+
+# single line comment with hash
+
+/*
+
+multiline comments
+between these star slash's
+
+
+*/
--- 02_variables.php
+++ 02_variables.php
@@ -1,25 +1,77 @@
 <?php
 
-// What is a variable
+// What is a variable : PHP is a loosley typed language - meaning you dont have to state the data type when you declare a variable
+
 
 // Variable types
 
+/* TYPES OF VARIABLE
+
+    STRING
+    INTEGER
+    FLOAT / DOUBLE
+    BOOLEAN
+    NULL
+    ARRAY
+    OBJECT
+    RESOURCE
+
+*/
+
 // Declare variables
+
+$name = 'Thomas'; // type of string
+$age  = 40;       // type of integer
+$isMale = true;   // type of boolean
+$height = 1.85;   // type of float / double
+$salary = null;   // type of NULL
 
 // Print the variables. Explain what is concatenation
 
+echo $name . '</br>';
+echo $age . '</br>';
+echo $isMale . '</br>'; // when boolean is converted into string true = 1 , false = "";
+echo $height . '</br>';
+echo $salary . '</br>';
+
 // Print types of the variables
+
+echo gettype($name) . '</br>'; // string
+echo gettype($age) . '</br>';   // integer
+echo gettype($isMale) . '</br>';    // boolean
+echo gettype($height) . '</br>';    // float / double
+echo gettype($salary) . '</br>';    // NULL
 
 // Print the whole variable
 
+echo '<pre>';
+var_dump($name, $height, $isMale, $salary, $age); // useful for printing out large amounts of data for debugging
+echo '<pre>';
+
 // Change the value of the variable
 
-// Print type of the variable
+$name = 'Daniel'; // variable reassignment
 
 // Variable checking functions
 
-// Check if variable is defined
+echo (is_string($name) ? 'true' : 'False') . '</br>';
+echo (is_integer($age) ? 'true' : 'False') . '</br>';
+echo (is_bool($isMale) ? 'true' : 'False') . '</br>';
+echo (is_double($height) ? 'true' : 'False') . '</br>';
 
-// Constants
+// Check if variable is defined - isset will check if the variable has a set value! it does nothing with the value - nor tell you the type.
+$address;
+echo (isset($name) ? 'true' : 'false') . '</br>';
+echo (isset($address) ? 'true' : 'false') . '</br>';
 
-// Using PHP built-in constants
+// Constants - cannot be changed define('NAME', VALUE);
+
+define('pi', 3.14);
+
+echo pi . '</br>'; // NO NEED FOR THE SOLLAR SIGN
+
+// Using PHP built-in constants already under the hood!
+
+echo SORT_ASC . '</br>';
+echo PHP_INT_MAX . '</br>';
+echo PHP_INT_MIN . '</br>';
