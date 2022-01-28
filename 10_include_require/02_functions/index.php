05f586dc68572e59477f8bbbe6cdcade2aef9792
Daniel Thomas <dan.thomas@playwerks.co.uk>
2022-01-28 09:34:05 +0000

    include/require

--- 10_include_require/02_functions/index.php
+++ 10_include_require/02_functions/index.php
@@ -1,1 +1,7 @@
 <?php
+
+require_once "math.php"; // use require for critical function / code where the rest of the code depends on it to run
+
+
+echo addTwo(1, 2) . '</br>';
+echo subtract(4, 2) . '</br>';
